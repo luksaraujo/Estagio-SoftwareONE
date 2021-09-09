209fa5ea6e61a702e775208976a04d1054aac966
Lucas Araujo <lucasarasouza@hotmail.com>
2021-09-09 18:07:57 -0300

    Cria o conteúdo do arquivo main da VM

---
+++ Terraform-Projects/Azure-Infrastructure-01/VM/main.tf
@@ -0,0 +1,42 @@
+provider "azurerm" {
+    version = "2.75.0"
+    subscription_id = var.subscriptionID
+
+    features {}
+}
+
+resource "azurerm_virtual_machine" "luksTestVM" {
+    name = "lukstestvm"
+    location = var.location
+    resource_group_name = var.resourceGroupName
+    network_interface_ids = ["${var.network_interface_id}"]
+    vm_size = "Standard_DS1_v2"
+
+    storage_image_reference {
+        publisher = "Canonical"
+        offer = "UbuntuServer"
+        sku = "18.04-LTS"
+        version = "latest"
+    }
+
+    storage_os_disk {
+        name = "disk1"
+        caching = "ReadWrite"
+        create_option = "FromImage"
+        managed_disk_type = "Standard_LRS"
+    }
+
+    os_profile {
+        computer_name = "lukstestVMdev01"
+        admin_password = "Test123"
+        admin_username = "azureuser"
+    }
+
+    os_profile_linux_config {
+        disable_password_authentication = false
+    }
+
+    tags = {
+      enviroment = "staging"
+    }
+}
